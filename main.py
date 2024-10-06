62baf7b40874b3253d3e6688020c6235dee6c8d1
HoDzha <hodzha@gmail.com>
2024-10-06 21:22:36 +0500

    Вывод цен на экран

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import style
-import random
+# Импорт необходимых библиотек
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+# Настройки для запуска браузера
 
-x = np.linspace(-10, 10, 100)
-y = x*x
-plt.plot(x, y)
-plt.xlabel('Ось Х')
-plt.ylabel('Ось У')
-plt.title('График функции y = x*x')
-plt.grid(True)
-plt.show()
+# Создание объекта драйвера
+
+driver = webdriver.Chrome()
+
+# Открытие страницы с предложениями аренды квартир
+url = "https://kazan.cian.ru/snyat-kvartiru-studiya-ili-1-komn/"
+driver.get(url)
+
+# Задержка, чтобы страница успела загрузиться
+time.sleep(10)
+
+# Парсинг цен квартир
+try:
+    # Используем XPath для нахождения цены
+    prices = driver.find_elements(By.XPATH, '//div[@data-name="PriceLayout"]//span[contains(@class, "aee5b7c44e--text--e4SBY")]')
+
+except Exception as e:
+    print(f'Произошла ошибка: {e}')
+finally:
+    pass
+    # Закрытие драйвера
+
+price_list = [price.text for price in prices]
+
+# Вывод полученных данных
+for i, price in enumerate(price_list, start=1):
+    print(f"Квартира {i}: {price}")
+
+# Закрытие браузера
+driver.quit()
+
+
+
