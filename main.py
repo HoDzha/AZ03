293a77400132aa14283a08819b89772d22d9180d
HoDzha <hodzha@gmail.com>
2024-10-06 20:33:48 +0500

    Update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 from matplotlib import style
 import random
 
-x = [1,2,3,4,5,6]
-y = [1,4,9,16,25,36]
-plt.style.use('ggplot')
-plt.plot(x,y)
-plt.title("Мой первый график")
-plt.xlabel("Ось X")
-plt.ylabel("Ось Y")
+x = np.linspace(-10, 10, 100)
+y = x*x
+plt.plot(x, y)
+plt.xlabel('Ось Х')
+plt.ylabel('Ось У')
+plt.title('График функции y = x*x')
+plt.grid(True)
 plt.show()
